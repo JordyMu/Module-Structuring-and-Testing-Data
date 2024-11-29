9ddfd7298bb52efec4d977a13e7d854c24cec0d5
JordyMu <jordymdev@gmail.com>
2024-11-30 01:12:17 +0200

    prediction and explaination for sprint-2 the folder errors and file 0.js

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -7,3 +7,15 @@ function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+
+// the error I cannot redeclare a variable (str) that already exists as a argument in scope function
+// the let str conflicts with existing variable.
+// How to Fix
+// I don't need to redeclare str the code will look like this:
+      
+      //  function capitalise(str) {
+      //  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+      //  return str;
+      //  }
+
+// I Removed the let and returns the capitalized result. there's no conflict.
