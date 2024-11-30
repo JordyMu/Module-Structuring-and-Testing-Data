e84e3dbb08a0c2a18b28d3df25b63fd6328c25ac
JordyMu <jordymdev@gmail.com>
2024-11-30 03:01:36 +0200

    prediction and explain for sprint-2 folder errors, file 2.js

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -7,4 +7,14 @@ function square(3) {
     return num * num;
 }
 
+// 3 cannot be used as parameter names this cause Error
+// num is used but is not defined anywhere
+
+// To Fix 
+// the valid code should be.
+// function square(num){
+//   return num * num;
+// }
+// console.log(square(3));
+
 
