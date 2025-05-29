ce2183cd2a181f73a6e3fd1c02887c9959e7d8c5
Jordy I Mukandayi <116026158+JordyMu@users.noreply.github.com>
2025-05-29 22:33:37 +0200

    describe what line 3 doing

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,6 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+//   = takes the value on the right(count + 1) store it in variable on the left (count)
+//   line 3 is incrementing
