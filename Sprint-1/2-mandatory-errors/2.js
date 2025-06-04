e151ed477a752756b3d022e9598503001bfb126e
Jordy I Mukandayi <116026158+JordyMu@users.noreply.github.com>
2025-06-04 21:32:37 +0200

    changing  console line

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
+// the console.log was above the declarationline
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
