896a3f19d1d60687c610ea46dc315b42e84f7b04
Jordy I Mukandayi <116026158+JordyMu@users.noreply.github.com>
2025-06-07 01:04:59 +0200

    "explain error of 1.js"

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,29 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+
+   /// The code throw errors due to redeclaring of the decimalNumber
+   /// The error is due because of redeclaring decimalNumber, using const results in a error
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
-  const percentage = `${decimalNumber * 100}%`;
+// function convertToPercentage(decimalNumber) {
+//   const decimalNumber = 0.5;
+//   const percentage = `${decimalNumber * 100}%`;
 
-  return percentage;
-}
+//   return percentage;
+// }
 
-console.log(decimalNumber);
+// console.log(decimalNumber);
 
 // =============> write your explanation here
 
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+/// decimaNumer is already declared as a function, then const decimalNumber = 0.5; trying also to redeclare it
+
+   
+//I have to remove the redeclaration of decimalNumber
+ Ex: function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage;
+}
+ console.log(convertToPercentage(0.5));
