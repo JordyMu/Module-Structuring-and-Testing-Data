e45705c2b4145d440327be4371f84f57a9aebf14
Jordy I Mukandayi <116026158+JordyMu@users.noreply.github.com>
2025-06-06 21:52:19 +0200

    "predict and explain error in 0.js"

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,23 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+   ///  The code will throw error because there is a variable redeclaration issue
+
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
 
 // =============> write your explanation here
+    
+   /// To fix I have to remove the let so that str is modified without being redeclared
+
 // =============> write your new code here
+
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
