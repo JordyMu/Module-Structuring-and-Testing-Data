647628fd164dd84ec125e76391322a275e755ef0
Jordy I Mukandayi <116026158+JordyMu@users.noreply.github.com>
2025-06-10 00:33:40 +0200

    "I fix with add num variable to the function" in 2.js"

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -1,24 +1,40 @@
 // Predict and explain first...
 
-// Predict the output of the following code:
-// =============> Write your prediction here
+/// The code should tell us the output will always be the last digit of each number.
+/// The explanation is the getLastDigit() is not working properly because  it completely ignores the parameter that's being passed to it
 
 const num = 103;
 
-function getLastDigit() {
+// function getLastDigit() {
+//   return num.toString().slice(-1);
+// }
+
+// console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+// console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+// console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+// Now run the code and compare the output to your prediction
+
+/// I run the code, it produces this.
+/// The last digit of 42 is 3
+/// The last digit of 105 is 3
+/// The last digit of 806 is 3
+
+// Explain why the output is the way it is
+
+/// getLastDigit() function supposed to return the last digit of a given number
+/// but it does not have any arguments
+/// it always refers to the global variable num, which is set to 103
+
+
+// Finally, correct the code to fix the problem
+
+/// To fix: I will add the num variable to the function
+
+function getLastDigit(num) {
   return num.toString().slice(-1);
 }
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
-
-// Now run the code and compare the output to your prediction
-// =============> write the output here
-// Explain why the output is the way it is
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-// =============> write your new code here
-
-// This program should tell the user the last digit of each number.
-// Explain why getLastDigit is not working properly - correct the problem
