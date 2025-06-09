e632f83d862f90f3dfb53de38f812ed4e8af861d
Jordy I Mukandayi <116026158+JordyMu@users.noreply.github.com>
2025-06-09 23:34:26 +0200

    "fix return on the same line with a + b"in 1.js

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,27 @@
 // Predict and explain first...
-//  =============> write your prediction here
 
-function sum(a, b) {
-  return;
-  a + b;
-}
+/// The code has a syntax error. The function have declaration sum with 2 arguments (a,b) the return statement ends the function immediately.
+/// Anything after return on a new line is ignored and returns undefined every time it is called.
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
+
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+
+/// The sum function has a return statement before a + b which means the function stoped immediately  and returns undefined,
+///    because a + b is unreachable.
+
 // Finally, correct the code to fix the problem
+
+/// To fix: Return statement should be on the same line with a + b.
+
 //  =============> write your new code here
+
+ function sum(a, b){
+  return a + b;
+ }
+ console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
