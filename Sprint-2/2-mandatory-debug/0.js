644fc5cae4e3bc7ece97e8004a36aa4598a298ba
Jordy I Mukandayi <116026158+JordyMu@users.noreply.github.com>
2025-06-09 21:39:44 +0200

    "replace first console.log to return statement in 0.js"

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,26 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+/// The prediction is  the code will print undefined in the string because it contains 2 console.log statements.
 
-function multiply(a, b) {
-  console.log(a * b);
-}
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
+
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
 
+/// The function declares multiply and take 2 arguments (a,b)
+/// the console.log of a*b will prints 320
+/// but console.log of The result of multiplying 10 and 32 is undefined, because  the function doesn't return a value.
+
+
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+
+/// To fix I will replace the first console statement: console.log(a * b); with a return statement: return a * b;
+
+function mutiply(a, b){
+  return a * b;
+}
+console.log(`The result of multiplying 10 and32 is ${multiply(10, 32)}`);
